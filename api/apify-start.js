047089cc34931280be6f01ad06949f56af6d74ff
guihsilvestro-sudo <guih.silvestro@outlook.com>
2026-03-20 18:36:56 -0300

    Update apify-start.js

--- api/apify-start.js
+++ api/apify-start.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
           username: [username],
-          resultsLimit: 200
+          resultsLimit: 30
         }),
       }
     );
